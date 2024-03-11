395ccf36a96d8f77ee4c07db04b89abfaffa6874
Erkin Tekin <erkintekin12@gmail.com>
2024-03-11 23:13:29 +0300

    some methods added and routes edited

--- client/auth.py
+++ client/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
 
-@auth.route('/login', method=['GET','POST'])
+@auth.route('/login', methods=['GET','POST']) # To allow get&post methods
 def login():
         return render_template("login.html")
 
@@ -11,6 +11,23 @@ def login():
 def logout():
         return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def signUp():
+        if request.method == 'POST':
+                email = request.form.get('email')
+                firstName = request.form.get('firstName')
+                password1 = request.form.get('password1')
+                password2 = request.form.get('password2')
+
+                if len(email) < 3:
+                        flash('E-mail must be greater than 2 characters.', category='error')
+                elif len(firstName) < 2:
+                        flash('First name must be greater than 1 characters.', category='error')
+                elif password1 != password2:
+                        flash('Password must match. Check again?', category='error')
+                elif len(password1) < 8:
+                        flash('Password must be at least 8 characters.', category='error')
+                else:
+                       # Successfully signed up
+                       flash('User created', category='success') 
         return render_template("sign_up.html")
